d9ab5cef8932a5cece7284bceac19fd91c5ddfd2
Gwali <gwalisam37@gmail.com>
2023-06-29 12:56:20 +0000

    getting file info

--- go-environment/main.go
+++ go-environment/main.go
@@ -1,18 +1,32 @@
 package main
 
 import (
-	"fmt"
-	"time"
 	"flag"
+	"fmt"
+	"os"
+	"time"
 )
 
 var (
-	name = flag.String("name","john", "what is your name")
+	name = flag.String("name", "john", "what is your name")
 )
 
 func main() {
 	fmt.Println(time.Now().Day())
 	flag.Parse()
 	fmt.Println(*name)
+
+	//getting file info
+	fileInfo, err := os.Stat("new.txt")
+	if err != nil {
+		fmt.Printf("%v", err)
+	}
+
+fmt.Println(fileInfo.Name())
+fmt.Println(fileInfo.ModTime())
+fmt.Println(fileInfo.IsDir())
+
+
+
 
 }
